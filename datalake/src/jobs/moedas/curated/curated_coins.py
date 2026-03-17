846804e88d4d10d3bf93855c3c7cdc6d2ebfddf0
Arthur Coutinho <arthurtelescoutinho@gmail.com>
2026-03-16 21:58:44 -0300

    feat: add curated tables

---
+++ datalake/src/jobs/moedas/curated/curated_coins.py
@@ -0,0 +1,85 @@
+from datetime import date
+from pyspark.sql import DataFrame, SparkSession
+import pyspark.sql.functions as F
+
+from .curated_table import CuratedCoinsTable
+from clients.spark_client import spark_client
+
+
+class CuratedCoins:
+    def __init__(self, date: date = date.today()):
+        self.date = date
+        self.table = CuratedCoinsTable()
+        self.spark: SparkSession = spark_client.get_session()
+
+    def get_data(self) -> DataFrame:
+        return self.spark.read.parquet(
+            f"/home/arthur/Arthur/Projetos/financial_pipeline/datalake/lakehouse/raw/raw_coins/data/dt_reference={self.date}"
+        )
+
+    def rename_columns(self) -> DataFrame:
+        coins = self.get_data()
+
+        coins = coins.select(
+            F.col("symbol").alias("symbol"),
+            F.col("paridadeCompra").alias("purchase_parity"),
+            F.col("paridadeVenda").alias("sales_parity"),
+            F.col("cotacaoCompra").alias("purchase_cotation"),
+            F.col("cotacaoVenda").alias("sales_cotation"),
+            F.col("dataHoraCotacao").alias("date_time_cotation"),
+            F.col("tipoBoletim").alias("bill_type"),
+            F.col("dt_reference"),
+        )
+
+        return coins
+
+    def spread_column(self, df: DataFrame) -> DataFrame:
+        return df.withColumn(
+            "spread",
+            F.round(
+                (F.col("sales_cotation") - F.col("purchase_cotation"))
+                / F.col("purchase_cotation"),
+                6,
+            ),
+        )
+
+    def apply_schema(self, df: DataFrame) -> DataFrame:
+        schema = self.table.schema()
+
+        return df.select(
+            *[
+                F.col(field.name).cast(field.dataType).alias(field.name)
+                for field in schema
+            ]
+        )
+
+    def create_table(self):
+
+        self.spark.sql(f"""
+            CREATE TABLE IF NOT EXISTS {self.table.full_name()} (
+                symbol STRING,
+                purchase_parity DOUBLE,
+                sales_parity DOUBLE,
+                purchase_cotation DOUBLE,
+                sales_cotation DOUBLE,
+                date_time_cotation TIMESTAMP,
+                bill_type STRING,
+                spread DOUBLE,
+                dt_reference DATE
+            )
+            USING iceberg
+            PARTITIONED BY (dt_reference)
+        """)
+
+    def save(self, df: DataFrame) -> None:
+        df.writeTo(self.table.full_name()).overwritePartitions()
+
+    def run(self):
+        df = self.rename_columns()
+        df = self.spread_column(df)
+        df = self.apply_schema(df)
+        self.create_table()
+        self.save(df)
+
+        print(df.show())
+        print(df.printSchema())
