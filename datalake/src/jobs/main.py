94ad0e9dbe4f1bacaccaf44d0092c5bc3125378a
Arthur Coutinho <arthurtelescoutinho@gmail.com>
2026-03-19 00:32:14 -0300

    feat: add raw-stocks

--- datalake/src/jobs/main.py
+++ datalake/src/jobs/main.py
@@ -1,14 +1,17 @@
 import sys 
 import os 
 
-from datetime import date
+from datetime import date, datetime
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from moedas.raw.raw_coins import RawCoins
 from moedas.curated.curated_coins import CuratedCoins
+from stocks.raw.stocks_job import RawStocksJob
 
 if __name__ == "__main__":
-    raw = RawCoins(date=date(2026, 3, 13))
+    # raw = RawCoins(date=date(2026, 3, 13))
+    # raw.run()
+    # curated = CuratedCoins(date=date(2026, 3, 13))
+    # curated.run()
+    raw = RawStocksJob(date=datetime(2026, 3, 13, 21, 24, 0))
     raw.run()
-    curated = CuratedCoins(date=date(2026, 3, 13))
-    curated.run()
